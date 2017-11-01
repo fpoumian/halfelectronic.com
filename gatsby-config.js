004c5e927a689760c576e22607115a4ad711716a
Fernando Poumián <fpoumian@gmail.com>
2017-10-31 18:02:45 -0700

    add custom site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Blog",
-    author: "Kyle Mathews",
+    title: "halfelectronic.com",
+    author: "Fernando Poumian",
   },
   plugins: [
     {
