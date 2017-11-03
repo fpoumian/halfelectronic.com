3612978e385477d2a8d6edc00cb0f4fa3562c800
Fernando Poumián <fpoumian@gmail.com>
2017-11-02 18:43:31 -0700

    remove markdown posts

--- src/components/CurrentlyReading/index.js
+++ src/components/CurrentlyReading/index.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import Book from "components/Book"
-import SidebarWidgetItem from "components/SidebarWidgetItem"
-import SidebarWidget from "components/SidebarWidget"
-import styles from "./index.module.css"
-import goodReadsLogo from "./goodreads.png"
+import Book from 'components/Book'
+import SidebarWidgetItem from 'components/SidebarWidgetItem'
+import SidebarWidget from 'components/SidebarWidget'
+import styles from './index.module.css'
+import goodReadsLogo from './goodreads.png'
 
 class CurrentlyReading extends Component {
   renderCurrentlyReadingBooks() {
     const { books } = this.props
     return (
       <div>
-        {books.map(({ title, link, authors }, index) =>
+        {books.map(({ title, link, authors }, index) => (
           <SidebarWidgetItem lastItem={index === books.length - 1} key={index}>
             <Book
               title={title[0]}
@@ -20,7 +20,7 @@ class CurrentlyReading extends Component {
               author={authors[0].author[0].name[0]}
             />
           </SidebarWidgetItem>
-        )}
+        ))}
       </div>
     )
   }
@@ -28,19 +28,19 @@ class CurrentlyReading extends Component {
   render() {
     const { loaded } = this.props
     const poweredBy = {
-      name: "GoodReads.com",
-      url: "https://www.goodreads.com/",
-      logo: goodReadsLogo
+      name: 'GoodReads.com',
+      url: 'https://www.goodreads.com/',
+      logo: goodReadsLogo,
     }
     return (
       <div className={styles.root}>
-        {!loaded
-          ? <span className={styles.loading}>
-              {"loading..."}
-            </span>
-          : <SidebarWidget poweredBy={poweredBy}>
-              {this.renderCurrentlyReadingBooks()}
-            </SidebarWidget>}
+        {!loaded ? (
+          <span className={styles.loading}>{'loading...'}</span>
+        ) : (
+          <SidebarWidget poweredBy={poweredBy}>
+            {this.renderCurrentlyReadingBooks()}
+          </SidebarWidget>
+        )}
       </div>
     )
   }
@@ -48,7 +48,7 @@ class CurrentlyReading extends Component {
 
 CurrentlyReading.propTypes = {
   loaded: PropTypes.bool.isRequired,
-  books: PropTypes.array.isRequired
+  books: PropTypes.array.isRequired,
 }
 
 export default CurrentlyReading
