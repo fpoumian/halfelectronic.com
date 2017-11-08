8e1772b2d558d276f210bebfa9d89f1e26b6135a
Fernando Poumián <fpoumian@gmail.com>
2017-11-08 11:35:02 -0700

    fix text in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -44,7 +44,7 @@ class About extends React.Component {
               </p>
               
               <p>
-                Nowadays, however, most of my stack is based around the ever growing Node.js/Javascript ecosystem. 
+                Nowadays, however, most of my stack is based around the ever growing Node.js and Javascript ecosystem. 
                 I'm a huge React enthusiast, and you will likely find me rambling about React at one point or another. 
                 This, in turn, has also sparked my interest in alternative programming and architectural paradigms 
                 such as Functional Programming, Isomorphic/Universal Javascript and Serverless Architectures.
