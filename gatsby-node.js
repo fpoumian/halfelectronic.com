cb0763499d7956a2ab7c400d76bb82a89974619b
Fernando Poumián <fpoumian@gmail.com>
2017-11-07 20:14:49 -0700

    fix lint and format

--- gatsby-node.js
+++ gatsby-node.js
@@ -172,34 +172,37 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
     return current
   })
 
+  return config
+
+  /*
   const features = {
     customProperties: {
       variables: {
-        // mainColor: 'blue',
-        // maxWidth: '43.5rem',
-        // accentColor: '#102b40',
-        // accentColorHover: '#009998',
-        // darkestGray: '#222326',
-        // darkGray: '#62646c',
-        // neutralGray: '#b2b2b2',
-        // lightGray: '#f6f6f6',
-        // lightestGray: '#f9f9f9',
-        // colorPrimary: '#373737',
-        // colorPrimaryDark: '#373737',
-        // colorSecondaryDark: '#080808',
-        // colorSecondary: '#080808',
-        // colorNeutralDark: '#111',
-        // colorNeutral: '#8C8D91',
-        // colorNeutralLight: '#FBFCFC',
-        // colorText: '#555',
-        // mainFontFamily: '"Varela Round", Arial, Sans-Serif',
-        // altFontFamily: '"Montserrat", Arial, Sans-Serif',
-        // contentFontFamily: '"Noto Serif","Times New Roman",Serif',
-        // contentFontSize: '1rem',
-        // contentLineHeight: '2rem',
-        // titleBaseFontSize: '2rem',
-        // titleBaseLineHeight: 'normal',
-        // excerptBaseFontSize: '1.1rem',
+        mainColor: 'blue',
+        maxWidth: '43.5rem',
+        accentColor: '#102b40',
+        accentColorHover: '#009998',
+        darkestGray: '#222326',
+        darkGray: '#62646c',
+        neutralGray: '#b2b2b2',
+        lightGray: '#f6f6f6',
+        lightestGray: '#f9f9f9',
+        colorPrimary: '#373737',
+        colorPrimaryDark: '#373737',
+        colorSecondaryDark: '#080808',
+        colorSecondary: '#080808',
+        colorNeutralDark: '#111',
+        colorNeutral: '#8C8D91',
+        colorNeutralLight: '#FBFCFC',
+        colorText: '#555',
+        mainFontFamily: '"Varela Round", Arial, Sans-Serif',
+        altFontFamily: '"Montserrat", Arial, Sans-Serif',
+        contentFontFamily: '"Noto Serif","Times New Roman",Serif',
+        contentFontSize: '1rem',
+        contentLineHeight: '2rem',
+        titleBaseFontSize: '2rem',
+        titleBaseLineHeight: 'normal',
+        excerptBaseFontSize: '1.1rem',
       },
     },
     customMedia: {
@@ -216,7 +219,7 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
   return config
 
   // custom postcss-next config
-  /*
+
   switch (stage) {
     case 'develop':
       config.merge({
