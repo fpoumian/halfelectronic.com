d82947bbc93108540d642d2d5430faa4ad3a1ef6
Fernando Poumián <fpoumian@gmail.com>
2017-11-22 22:44:16 -0700

    Remove needless components.

--- src/components/HomepageHeaderButtons/index.js
+++ src/components/HomepageHeaderButtons/index.js
@@ -37,32 +37,6 @@ class HomepageHeaderButtons extends Component {
     )
   }
 }
-
-const __HomepageHeaderButtons = ({ headingTyped }) => {
-  const visibility = {
-    visibility: headingTyped ? 'visible' : 'hidden',
-    opacity: headingTyped ? '1' : '0',
-  }
-
-  const scrollToBlogSection = event => {
-    if (!scrollToElement) return event
-    event.preventDefault()
-    scrollToElement('#posts-list')
-  }
-
-  return (
-    <div className={styles.wrapper} style={visibility}>
-      <Tag bgStyle="dark" label="About" link="/about" />
-      <Tag
-        bgStyle="light"
-        label="Read Blog"
-        link="#"
-        onClick={scrollToBlogSection}
-      />
-    </div>
-  )
-}
-
 HomepageHeaderButtons.propTypes = {
   headingTyped: PropTypes.bool,
 }
